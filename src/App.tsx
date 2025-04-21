00eaf8db7439a52018600ae9dbdf31ac74c218ed
Joanne <joanne.zh2015@gmail.com>
2025-04-22 01:19:03 +0800

    decorate menu page and first deploy on firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
+import {
+  Button,
+  styled
+} from '@mui/material'
 import './App.css';
+
+const MenuButton = styled(Button)({
+  width: 'auto',
+  fontSize: 16,
+  zIndex: 2,
+  backgroundColor: '#B97550',
+  borderRadius: '50px',
+  marginTop: '20px',
+  transition: 'marginBottom 3s',
+  '&:hover': {
+    backgroundColor: '#AF6B46',
+    boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.5)',
+    transform: 'translateY(-10px)',
+    transition: '0.1s',
+  }
+});
 
 function App() {
   return (
-    <body className="App">
+    <div className="App">
       <div className="App-header">
         <div className="wave"></div>
         <div className="wave"></div>
         <div className="wave"></div>
       </div>
-    </body>
+
+      <div className="Nav-bar">
+        Nav Bar
+      </div>
+
+      <div className="Menu-Content">
+        <h1 className="Title">
+          Start your journey with us
+        </h1>
+
+        <MenuButton
+          variant='contained'
+        >
+          Get Started
+        </MenuButton>
+      </div>
+    </div>
   );
 }
 
---
+++ src/config.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyABQdJvbzbxOVupwHXAeUnmJMa3CtaR0W4",
+    authDomain: "midtermproject-112062309.firebaseapp.com",
+    projectId: "midtermproject-112062309",
+    databaseURL: "https://midtermproject-112062309-default-rtdb.firebaseio.com/",
+    storageBucket: "midtermproject-112062309.firebasestorage.app",
+    messagingSenderId: "893224602038",
+    appId: "1:893224602038:web:ee948cb7a2c7034fad5917",
+    measurementId: "G-FGPLD3L9ZN"
+};
+
+// Initialize Firebase
+const config = initializeApp(firebaseConfig);
+export default config;
