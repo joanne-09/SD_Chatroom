13f757c91441670aaa125c937f7812994f7446b9
Joanne <joanne.zh2015@gmail.com>
2025-04-23 21:44:31 +0800

    put api key to .env

--- src/config.tsx
+++ src/config.tsx
@@ -9,14 +9,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyABQdJvbzbxOVupwHXAeUnmJMa3CtaR0W4",
-    authDomain: "midtermproject-112062309.firebaseapp.com",
-    projectId: "midtermproject-112062309",
-    databaseURL: "https://midtermproject-112062309-default-rtdb.firebaseio.com/",
-    storageBucket: "midtermproject-112062309.firebasestorage.app",
-    messagingSenderId: "893224602038",
-    appId: "1:893224602038:web:ee948cb7a2c7034fad5917",
-    measurementId: "G-FGPLD3L9ZN"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
